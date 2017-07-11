ce875a9bbb9579512321b799bba4f7fdff3dc5fb
smmakowski <makowski.sm@gmail.com>
2017-07-11 16:10:52 -0700

    Modularize dea-calc into helper functions

--- dea-calc.js
+++ dea-calc.js
@@ -1,52 +1,111 @@
-function deaCheck(num) {
+// Validation Helpers
+function areLetters(str) {
+  const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  for (let i = 0; i < str.length; i++) {
+    if (letters.indexOf(str[i]) === -1) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function areDigits(str) {
+  const digits = '1234567890';
+  for (let i = 0; i < str.length; i++) {
+    if (digits.indexOf(str[i]) === -1) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function calcComparison(nums) {
+  //get sums
+  const step1 = nums[0] + nums[2] + nums[4];
+  const step2 = (nums[1] + nums[3] + nums[5]) * 2;
+
+  // process comparison number
+  let step3 = step1 + step2;
+  step3 = step3.toString();
+  step3 = step3[step3.length - 1];
+  step3 = parseInt(step3);
+}
+
+// Main algorithm
+function isValidType(str) {
   const regType = {
     a: 'Hospital/Prescriber/Pharmacy (deprecated)',
     b: 'Hospital/Clinic',
+    c: 'Practitioner',
+    d: 'Teaching Institution',
+    e: 'Manufacturer',
     f: 'Distributor',
     g: 'Researcher',
+    h: 'Analytical Lab',
+    j: 'Importer',
+    k: 'Exporter',
+    l: 'Reverse Distributor',
     m: 'Mid Level Practitioner',
     p: 'Narcotic Treatment Program',
     r: 'Narcotic Treament Program',
+    s: 'Narcotic Treament Program',
+    t: 'Narcotic Treament Program',
+    u: 'Narcotic Treament Program',
     x: 'Suboxone/Subutex Prescribing Program',
+  };
+
+  if (regType[str.toLowerCase()] === undefined) {
+    return false;
   }
 
+  return true;
+}
+
+function deaCheck(num) {
   // process nums
-  let chars = num.split('');
-  const type = regType[num[0].toLowerCase()];
-  const lastInitial = num[1];
+  const chars = num.split('');
+  const type = num[0].toLowerCase();
+  const lastInitial = num[1].toLowerCase();
   const check = parseInt(chars[chars.length - 1]);
   const nums = chars.slice(2, chars.length - 1).map((n) => {
     return parseInt(n);
   });
+
   console.log('processed nums are: ', nums);
-  // invalid lengths
-  // if (num.length !== 9 || nums.length !== 6) {
-  //   return 'Invalid length';
-  // }
-  // // invalid type
-  // if (!type) {
-  //   return 'Not recognized type!'
-  // }
-
-  // calculate sums
-  const step1 = nums[0] + nums[2] + nums[4];
-  const step2 = (nums[1] + nums[3] + nums[5]) * 2;
-  console.log('Step 1 is...', step1);
-  console.log('Step 2 is... ', step2);
-
-  // process comparison number
-  let comp = step1 + step2;
-  comp = comp.toString();
-  comp = comp[comp.length - 1];
-  comp = parseInt(comp);
-
-  // validate comparison number
-  if (comp === check) {
-    return true;
+  // invalid format
+  if (num.length !== 9 || nums.length !== 6) {
+    return 'length';
+  }
+  if (!areLetters(num.slice())) {
+    return 'letter';
   }
 
-  return false;
+  if (!areDigits(num.slice(2))) {
+    return 'num';
+  }
+
+  // invalid type
+  if (!isValidType(type)) {
+    return 'type';
+  }
+
+  const comp = calcComparison(nums);
+  // validate step3 with comparison number
+  if (comp === check) {
+    return 'valid';
+  }
+
+  return 'comp';
 
 }
 
-module.exports.deaCheck = deaCheck;
+// Function Export
+module.exports = {
+  deaCheck: deaCheck,
+  areLetters: areLetters,
+  areDigits: areDigits,
+  isValidType: isValidType,
+  calcComparison: calcComparison,
+};
