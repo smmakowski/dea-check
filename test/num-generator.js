9d3cdb9e2d1888135ea4ecbe676e349c9d686510
smmakowski <makowski.sm@gmail.com>
2017-07-12 18:14:29 -0700

    (test) create tests for random number generator

---
+++ test/num-generator.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+const chai = require('chai');
+const expect = chai.expect;
+
+const validator = require('../dea-calc.js');
+const generator = require('../num-generator.js');
+
+describe('DEA Number Generator', () => {
+    it('should return a random DEA number as a string',
+    () => {
+      expect((typeof generator.generateNumber()).to.equal('string');
+    });
+    it('should return a valid DEA number when checked by the validator function',
+    () => {
+      const rand = generator.generateNumber();
+      const last = rand[1];
+      expect(validator.deaCheck(rand, last)).to.equal('valid');
+    });
+  });
+
+});
